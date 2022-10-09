3c7711a545fd27f3fb701fc7c0d4301646715695
arumhatre1318 <arushimhatre1318@gmail.com>
2022-10-09 17:44:42 +0530

    Color Cub

---
+++ js/cub.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".container").addEventListener('click', function(e) {
+	//console.log(e.target.innerHTML);
+	//alert("jhkjsd");
+	document.body.style.backgroundColor = e.target.style.backgroundColor;
+})
